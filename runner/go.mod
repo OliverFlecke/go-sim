76856ee3e7c5fabdd1cf5759de23e14cdfa35804
Oliver Fleckenstein <oliverfl@live.dk>
2022-08-02 22:09:48 +0100

    feat: Simple terminal UI to render and move agent around

--- runner/go.mod
+++ runner/go.mod
@@ -5,3 +5,9 @@ go 1.18
 replace simulator/core => ../core
 
 require simulator/core v0.0.0-00010101000000-000000000000
+
+require (
+	atomicgo.dev/keyboard v0.2.8 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+)
--- runner/runner.go
+++ runner/runner.go
@@ -3,15 +3,49 @@ package main
 import (
 	"fmt"
 	simulator "simulator/core"
+
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
 )
 
 func main() {
-	fmt.Println("Running simulation")
+	fmt.Println("Starting simulation...")
+	world := simulator.NewGridWorld(10)
 	agent := simulator.NewAgent("Agent A")
-	moves := []simulator.Direction{simulator.NORTH, simulator.EAST, simulator.EAST, simulator.EAST}
+	fmt.Print(world.ToStringWithAgents([]simulator.Agent{*agent}))
 
-	for _, d := range moves {
-		agent.Move(d)
-		fmt.Println(agent)
+	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+		if key.Code == keys.CtrlC {
+			return true, nil
+		}
+
+		dir, found := keyToDirection(key)
+		if found {
+			agent.Move(dir)
+			clearScreen()
+			fmt.Print(world.ToStringWithAgents([]simulator.Agent{*agent}))
+		}
+
+		return false, nil
+	})
+}
+
+func clearScreen() {
+	fmt.Println("\033[2J")
+}
+
+func keyToDirection(key keys.Key) (simulator.Direction, bool) {
+	switch key.Code {
+	case keys.Right:
+		return simulator.EAST, true
+	case keys.Left:
+		return simulator.WEST, true
+	case keys.Up:
+		return simulator.SOUTH, true
+	case keys.Down:
+		return simulator.NORTH, true
+
+	default:
+		return 0, false
 	}
 }
