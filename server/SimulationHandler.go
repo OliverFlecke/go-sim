3c94bebfef752183819ddd6cc92bac920b3331c4
Oliver Fleckenstein <oliverfl@live.dk>
2022-08-27 19:28:05 +0100

    wip: getting SSE to work

--- server/SimulationHandler.go
+++ server/SimulationHandler.go
@@ -21,37 +21,34 @@ func NewSimulationHandler() *SimulationHandler {
 	}
 }
 
-func (h *SimulationHandler) ServeHttp(res http.ResponseWriter, req *http.Request) {
-	var head string
-	head, req.URL.Path = ShiftPath(req.URL.Path)
+func (h *SimulationHandler) ServeHttp(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("[APP] %s %s %s\n", time.Now().UTC().Format(time.RFC3339), r.Method, r.URL.Path)
 
-	if head == "create" && req.Method == http.MethodPost {
+	var head string
+	head, r.URL.Path = ShiftPath(r.URL.Path)
+
+	if head == "create" && r.Method == http.MethodPost {
 		// TODO: Should return error if map cannot be found
 		// TODO: Should read map from request body
 		id, _ := h.startSimulation("04.map")
-		res.Write([]byte(id))
+		w.Write([]byte(id))
 		return
 	}
 
 	sim := h.simulations[head]
 	if sim == nil {
-		http.Error(res, fmt.Sprintf("invalid simulation id %q", head), http.StatusBadRequest)
+		http.Error(w, fmt.Sprintf("invalid simulation id %q", head), http.StatusBadRequest)
 		return
 	}
 
-	head, req.URL.Path = ShiftPath(req.URL.Path)
+	head, r.URL.Path = ShiftPath(r.URL.Path)
 	switch head {
-	case "map":
+	case "stream":
+		h.streamEvents(sim).ServeHTTP(w, r)
+	case "agent": // TODO:
 
-	case "raw":
-		name := sim.GetWorld().GetName()
-		content, err := os.ReadFile(filepath.Join(PATH_TO_LEVELS, name))
-		if err != nil {
-			fmt.Println(err.Error())
-			http.Error(res, fmt.Sprintf("no level found for simulation %s", sim.Id), http.StatusBadRequest)
-			return
-		}
-		res.Write([]byte(content))
+	case "map":
+		h.sendRawLevelContent(sim).ServeHTTP(w, r)
 	}
 }
 
@@ -72,4 +69,39 @@ func (h *SimulationHandler) startSimulation(levelName string) (string, error) {
 	h.simulations[id] = sim
 
 	return id, nil
+}
+
+func (h *SimulationHandler) sendRawLevelContent(sim *simulator.Simulation) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		name := sim.GetWorld().GetName()
+		content, err := os.ReadFile(filepath.Join(PATH_TO_LEVELS, name))
+		if err != nil {
+			fmt.Println(err.Error())
+			http.Error(w, fmt.Sprintf("no level found for simulation %s", sim.Id), http.StatusBadRequest)
+			return
+		}
+		w.Write([]byte(content))
+	})
+}
+
+func (h *SimulationHandler) streamEvents(sim *simulator.Simulation) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		flusher, _ := w.(http.Flusher)
+
+		for e := range sim.GetEvents() {
+			switch e.Status {
+			case simulator.COMPLETED:
+				fmt.Fprint(w, "complete")
+				flusher.Flush()
+				return
+			case simulator.RUNNING:
+				fmt.Fprintf(w, "data: %s\n\n", e.CurrentTime)
+				flusher.Flush()
+			}
+		}
+	})
 }
